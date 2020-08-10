13e08494e2ece48311b3621ef5ccdd13dabee083
BimaliWickramasinghe <bimalicw@gmail.com>
2020-08-10 13:42:52 +0530

    readFile class finished. Exception handling for invalid format is fixed.

--- SimpleCalculator/src/bw/Main.java
+++ SimpleCalculator/src/bw/Main.java
@@ -5,7 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		readFile myfile = new readFile();
-		
+		if (myfile.readMyFile()==true) {
+			System.out.println("What operation do you want to perform? :");
+			operate newOp = new operate();
+		}else {
+			System.out.println("Fix the above error");
+		}
 	}
 
 }
--- SimpleCalculator/src/bw/readFile.java
+++ SimpleCalculator/src/bw/readFile.java
@@ -3,25 +3,43 @@ import java.util.*;
 import java.io.*;
 
 public class readFile{
-	//constructor of superclass readFile
-	readFile(){
+	//file content
+	int[] num = new int[2];
+	int count=0;
+	
+	//checks whether file exists
+	public boolean readMyFile(){
+		boolean validity = false;
 		try {
 			File myFile = new File("src\\bw\\myFile.txt");
 			Scanner sc = new Scanner (myFile);
 			while(sc.hasNext()) {
-				check(sc.next());
+				if(check(sc.next())==true) {
+					validity = true;
+					continue;
+				}else {
+					validity = false;
+					break;
+				}
 			}
 			sc.close();
+			return (validity);
 		}
 		catch (FileNotFoundException e){
 			System.out.println("File doesnot exist");
+			return false;
 		}	
-		
 	}
 
-	
 	//Checks the validity of the content
-	public void check(String sc) {
-		System.out.println(sc);
+	public boolean check(String sc) {
+		try {
+			num[count] = Integer.parseInt(sc);
+			count +=1;
+			return true;
+		}catch(NumberFormatException e) {
+			System.out.println("Invalid format");
+			return false;
+		}
 	}
 }
