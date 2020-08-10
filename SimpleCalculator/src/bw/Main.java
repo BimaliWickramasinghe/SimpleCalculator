fc995183ec40810f4abd572d57b8726710b59513
BimaliWickramasinghe <bimalicw@gmail.com>
2020-08-10 16:23:58 +0530

    colloborate and finished

--- SimpleCalculator/src/bw/Main.java
+++ SimpleCalculator/src/bw/Main.java
@@ -1,16 +1,19 @@
 package bw;
+import java.util.*;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		readFile myfile = new readFile();
-		if (myfile.readMyFile()==true) {
-			System.out.println("What operation do you want to perform? :");
-			operate newOp = new operate();
+		readFile myfile = new readFile();            //create object of class readFile
+		Scanner myInput = new Scanner(System.in);    //create object to get the output
+		
+		if (myfile.readMyFile()==true) {             //check whether file exists and its validity
+			System.out.println("Type the arithmetic operator to perform a calculation : ");
+			String op = myInput.nextLine();          //takes the user input
+			myfile.oper(op);                         //do the operations
+			myInput.close();
 		}else {
-			System.out.println("Fix the above error");
+			System.out.println("Fix the above error");    //ny error is informed
 		}
 	}
-
 }
