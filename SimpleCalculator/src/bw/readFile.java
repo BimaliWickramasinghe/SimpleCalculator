026e3624e7821ee598dd4f3db280974526177727
BimaliWickramasinghe <bimalicw@gmail.com>
2020-08-10 16:23:10 +0530

    pass values to operate class

--- SimpleCalculator/src/bw/readFile.java
+++ SimpleCalculator/src/bw/readFile.java
@@ -2,30 +2,32 @@ package bw;
 import java.util.*;
 import java.io.*;
 
-public class readFile{
-	//file content
+public class readFile extends operate{
 	int[] num = new int[2];
 	int count=0;
 	
-	//checks whether file exists
-	public boolean readMyFile(){
+	public boolean readMyFile(){                   //check whether file exists and its validity
 		boolean validity = false;
 		try {
-			File myFile = new File("src\\bw\\myFile.txt");
-			Scanner sc = new Scanner (myFile);
-			while(sc.hasNext()) {
-				if(check(sc.next())==true) {
+			File myFile = new File("src\\bw\\myFile.txt"); //open file
+			Scanner sc = new Scanner (myFile);      //read line
+			while(sc.hasNext()) {                   //check validity
+				if(check(sc.next())==true) {        //if the content is an integer validity is true
 					validity = true;
 					continue;
-				}else {
+				}else {                             // if the content is not an integer validity is false
 					validity = false;
 					break;
 				}
 			}
 			sc.close();
+			System.out.println("Your file contains;");
+			show();                                //calls show() to show the file content
+			this.setNum1(num[0]);                  //pass number 1 to operate class
+			this.setNum2(num[1]);                  //pass number 2 to operate class 
 			return (validity);
 		}
-		catch (FileNotFoundException e){
+		catch (FileNotFoundException e){           //handles FileNotFoundException
 			System.out.println("File doesnot exist");
 			return false;
 		}	
@@ -34,12 +36,20 @@ public class readFile{
 	//Checks the validity of the content
 	public boolean check(String sc) {
 		try {
-			num[count] = Integer.parseInt(sc);
-			count +=1;
+			num[count] = Integer.parseInt(sc);     //stores the value in an array
+			count +=1;                             //increment the counter by one
+			if(count>2) {                          //warn the user if there are more than two lines
+				System.out.println("There are more than two lines in the file");
+				return false;
+			}
 			return true;
-		}catch(NumberFormatException e) {
+		}catch(NumberFormatException e) {          //errors cause to invalid formats are handled 
 			System.out.println("Invalid format");
 			return false;
 		}
+	}
+	public void show() {                          //Shows the file content
+		System.out.println(num[0]);
+		System.out.println(num[1]);
 	}
 }
