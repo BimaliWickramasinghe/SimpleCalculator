e706c130aaa1ddf0e411cd56f117088d76aba733
BimaliWickramasinghe <bimalicw@gmail.com>
2020-08-10 16:23:32 +0530

    do the operations

--- SimpleCalculator/src/bw/operate.java
+++ SimpleCalculator/src/bw/operate.java
@@ -1,5 +1,40 @@
 package bw;
 
-public class operate extends readFile {
+public class operate{
+	int a;
+	int b;
+	int ans;
 
+	public void setNum1(int num1) {            //stores number 1
+		this.a= num1;
+	}
+	public void setNum2(int num1) {            //stores number 2
+		this.b= num1;
+	}
+
+	public void oper(String op) {              //do arithmetic operations
+		try {
+			switch (op) {
+			case "-":
+				ans= a-b;
+				break;
+			case "+":
+				ans= a+b;
+				break;
+			case "/":
+				ans= a/b;
+				break;
+			case "*":
+				ans= a*b;
+				break;
+			case "%":
+				ans= a%b;
+			default:                          //handles invalid operators
+				System.out.println("Invalid operator");
+			}
+			System.out.println("Your answer is : "+ ans);  //shows the answer
+		}catch(ArithmeticException e) {        //handles any arithmetic exceptions
+			System.out.println(e);
+		}
+	}
 }
