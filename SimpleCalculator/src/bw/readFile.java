3d20e80174a53df1711232602f4c3064059550ec
BimaliWickramasinghe <bimalicw@gmail.com>
2020-08-10 12:07:55 +0530

    readFile class updated

--- SimpleCalculator/src/bw/readFile.java
+++ SimpleCalculator/src/bw/readFile.java
@@ -1,5 +1,27 @@
 package bw;
+import java.util.*;
+import java.io.*;
 
-public class readFile {
+public class readFile{
+	//constructor of superclass readFile
+	readFile(){
+		try {
+			File myFile = new File("myFile.txt");
+			Scanner sc = new Scanner (myFile);
+			while(sc.hasNext()) {
+				check(sc.next());
+			}
+			sc.close();
+		}
+		catch (FileNotFoundException e){
+			System.out.println("File doesnot exist");
+		}	
+		
+	}
 
+	
+	//Checks the validity of the content
+	public void check(String sc) {
+		System.out.println(sc);
+	}
 }
